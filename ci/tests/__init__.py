2ed18247a7b37794c420d99b12473ed82d08a616
Joshua Thompson-Lindley <joshua@overflow.digital>
2023-01-04 15:17:20 +0000

    Fix typeerror

--- ci/tests/__init__.py
+++ ci/tests/__init__.py
@@ -3,6 +3,7 @@ from functools import reduce
 from math import ceil, floor
 
 from cuid2 import cuid
+from cuid2.utils import string_to_int
 
 
 def id_to_b36(id: int) -> int:
@@ -42,7 +43,7 @@ async def create_id_pool(max: int = 100000) -> tuple[list[int], list[int], list[
             break
 
     id_pool: list[int] = list(id_pool_set)
-    numbers: list[int] = list(map(lambda id: id_to_b36(id), id_pool))
+    numbers: list[int] = list(map(lambda id: id_to_b36(string_to_int(id)), id_pool))
     histogram: list[int] = build_histogram(numbers)
 
     return (id_pool, numbers, histogram)
